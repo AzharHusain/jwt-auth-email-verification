32c5b42f4b66fc011fac0e1e722d3d9aebeb9a96
Azhar Husain <kontactazhar@live.com>
2018-06-07 15:40:26 +0530

    final

---
+++ backend/environment.js
@@ -0,0 +1,6 @@
+module.exports =  {
+    "MJ_APIKEY_PUBLIC": "<your public key>",
+    "MJ_APIKEY_PRIVATE": "<your private key>",
+    "MJ_SENDER_MAIL": "<your registered email>",
+    "MJ_SENDER_NAME": "<any custom name for email>"
+};
